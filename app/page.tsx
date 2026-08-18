125e20bd828e8a1a9b407d8a7dc2b28f51408cbe
Risboroughgm <sales@risboroughgm.co.uk>
2026-08-18 16:15:01 +0100

    Link DealerCore test tools from app

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
+import Link from "next/link";
 import DealerCoreApp from "./DealerCoreApp";
 
 export default function Home() {
-  return <DealerCoreApp />;
+  return (
+    <>
+      <DealerCoreApp />
+      <Link
+        href="/tools"
+        className="fixed bottom-5 right-5 z-40 rounded-full border border-[#cbd6c9] bg-white px-4 py-2 text-sm font-semibold text-[#376f2b] shadow-lg hover:bg-[#f4f6f3] print:hidden"
+      >
+        Data & Test Tools
+      </Link>
+    </>
+  );
 }
